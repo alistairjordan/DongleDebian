bbc19d1dfcb8665bbed1d673d370694e0b6a0333
alistairjordan <>
2023-10-27 20:48:27 +0100

    Convert to Ubuntu runner

--- scripts/docker_brun.sh
+++ scripts/docker_brun.sh
@@ -1,2 +1,2 @@
-docker build -t dongle .
-docker run -it  --privileged --mount type=bind,source="$(pwd)",target=/dongle dongle build
+sudo docker build -t dongle .
+sudo docker run -it  --privileged --mount type=bind,source="$(pwd)",target=/dongle dongle build
