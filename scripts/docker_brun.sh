d4f9b0fb4c04049b66c7091cdbb756e3d86dc589
alistairjordan <>
2023-10-27 20:58:08 +0100

    Upload artifacts

--- scripts/docker_brun.sh
+++ scripts/docker_brun.sh
@@ -1,2 +1,2 @@
 sudo docker build -t dongle .
-sudo docker run -it  --privileged --mount type=bind,source="$(pwd)",target=/dongle dongle build
+sudo docker run --privileged --mount type=bind,source="$(pwd)",target=/dongle dongle build
